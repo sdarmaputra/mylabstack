4e9988e32c342fb965cce951a84bc09a292d498d
sdarmaputra <surya.igede@gmail.com>
2016-03-13 03:15:29 +0000

    add user list of laboratory

--- app_modules/lab/controller/labController.js
+++ app_modules/lab/controller/labController.js
@@ -52,7 +52,52 @@ router.get('/destroy/:idlab', function(req, res, next) {
 	});
 });
 
-/* GET home page. */
+router.get('/userList/:idlab', function(req, res, next) {
+	lab.findOne({ include: [user], where: { idlab: req.params.idlab } }).then(function(lab) {
+		data.users = lab.users;
+		data.idlab = lab.idlab;
+		data.selected = [];
+		data.url = '/lab/userList/'+req.params.idlab+'/add';
+		user.findAll().then(function(users) {
+			data.userList = [];
+			data.userList.push({value: 0, text: '- Select User(s) -'});
+			users.forEach(function(user, index, arr) {
+				var arrayUser = {};
+				arrayUser['value'] = user.iduser;
+				arrayUser['text'] = user.username + ' (' + user.full_name + ')';
+				data.userList.push(arrayUser);
+			});
+			res.render(path.join(view_dirname, 'userList'), data);
+		});
+	});
+});
+
+router.post('/userList/:idlab/add', function(req, res, next) {
+	var userList =[];
+	userList = req.body.userList;
+	console.log(req.body);
+	lab.findOne({ where: { idlab: req.params.idlab } }).then(function(lab) {
+		var itemProcessed = 0;
+		userList.forEach(function(iduser, index, array) {
+			user.findOne({ where: { iduser: iduser } }).then(function(user) {
+				lab.addUser(user).then(function() {
+					itemProcessed = checkProcessCompletion(itemProcessed, array, '/lab/userList/'+req.params.idlab, res);
+				});
+			});
+		});
+	});
+});
+
+router.get('/userList/:idlab/remove/:iduser', function(req, res, next) {
+	lab.findOne({ where: { idlab: req.params.idlab } }).then(function(lab) {
+		user.findOne({ where: { iduser: req.params.iduser } }).then(function(user) {
+			lab.removeUser(user).then(function() {
+				res.redirect('/lab/userList/'+req.params.idlab);
+			});
+		});
+	});
+});
+
 router.get('/getLabs', function(req, res, next) {
 	lab.findAll({ include: [user] }).then(function(labs) {
 		res.json(labs);
@@ -68,5 +113,15 @@ router.get('/getLab/:name', function(req, res, next) {
 router.get('/', function(req, res, next) {
 	res.render(path.join(view_dirname, 'index'), data);
 });
+
+// Functions
+function checkProcessCompletion(itemProcessed, array, redirectUrl, res) {
+	itemProcessed++;
+	if (itemProcessed === array.length) {
+		res.redirect(redirectUrl);
+	} else {
+		return itemProcessed;
+	}
+}
 
 module.exports = router;
